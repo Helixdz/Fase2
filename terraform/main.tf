eeeebd1b014487b3f8042701c2580986fbef178a
Raul Cristo <raulcristomartinez@gmail.com>
2026-03-01 22:48:35 -0600

    trigger CI pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,8 +82,8 @@ module "eks" {
     default = {
       instance_types = [var.node_instance_type]
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 1
+      desired_size   = 1
     }
   }
 
